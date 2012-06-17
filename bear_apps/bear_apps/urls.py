27b68598accd0147971aa354848faa315d1cec45
Doug Cook <douglasc92@eecs.berkeley.edu>
2012-06-16 19:08:07 -0700

    created the default view in views.py

--- bear_apps/bear_apps/urls.py
+++ bear_apps/bear_apps/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'bear_apps.views.home', name='home'),
+    url(r'^$', 'store.views.home', name='home'),
     # url(r'^bear_apps/', include('bear_apps.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
