b7f34bebb959108b6af42014c31767fa56606412
Ronald Lee <rwlee5168@gmail.com>
2012-07-11 16:49:15 -0700

    Updated urls.py and views.py with new views for browse and my-apps. Also added those .html templates.

--- bear_apps/store/views.py
+++ bear_apps/store/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 def home(request):
     return render_to_response('index.html')
+
+def browse(request):
+    return render_to_response('browse.html')
+
+def myapps(request):
+    return render_to_response('my-apps.html')
