302f2966d4f77606f1ed5f7597aa2220d7074c2c
Ronald Lee <rwlee5168@gmail.com>
2012-07-11 10:31:50 -0700

    Changed to render_to_response form from the longer form

--- bear_apps/store/views.py
+++ bear_apps/store/views.py
@@ -1,11 +1,6 @@
 # Create your views here.
 from django.template import Context, loader
 from django.http import HttpResponse
-
+from django.shortcuts import render_to_response
 def home(request):
-    # TODO Change this code to use render_to_response() to cut down on
-    # boilerplate code when making basic views:
-    # https://docs.djangoproject.com/en/dev/intro/tutorial03/#a-shortcut-render-to-response
-    t = loader.get_template('index.html')
-    c = Context()
-    return HttpResponse(t.render(c))
+    return render_to_response('index.html')
