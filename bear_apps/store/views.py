f1a2bfffc457bc04c7ad6bc22417c0081c8ed98f
Aaron Culich <aculich@eecs.berkeley.edu>
2012-07-10 19:59:56 -0700

    added TODO comment for render_to_response
    Change this code to use render_to_response() to cut down on #
    boilerplate code when making basic views:
    https://docs.djangoproject.com/en/dev/intro/tutorial03/#a-shortcut-render-to-response

--- bear_apps/store/views.py
+++ bear_apps/store/views.py
@@ -3,6 +3,9 @@ from django.template import Context, loader
 from django.http import HttpResponse
 
 def home(request):
+    # TODO Change this code to use render_to_response() to cut down on
+    # boilerplate code when making basic views:
+    # https://docs.djangoproject.com/en/dev/intro/tutorial03/#a-shortcut-render-to-response
     t = loader.get_template('index.html')
     c = Context()
     return HttpResponse(t.render(c))
