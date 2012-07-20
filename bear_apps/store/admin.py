828850ea7e246177ef7909c3bf6e593b1ec58e7e
Ronald Lee <rwlee5168@gmail.com>
2012-07-20 15:58:40 -0700

    Edited admin for chartstring

--- bear_apps/store/admin.py
+++ bear_apps/store/admin.py
@@ -1,14 +1,17 @@
-from store.models import User_Apps, User, App, Group
+from store.models import User_Apps, User, App, Group, Chartstring
 from django.contrib import admin
 
 class AppsInline(admin.TabularInline):
 	model = User_Apps
-	extra = 3
+
+class ChartstringInline(admin.TabularInline):
+	model = Chartstring
 
 class UserAdmin(admin.ModelAdmin):
-	inlines= [AppsInline]
+	inlines = [AppsInline]
 
 admin.site.register(User, UserAdmin)
 admin.site.register(App)
 admin.site.register(Group)
+admin.site.register(Chartstring)
 # admin.site.register(User)
