2f70103c63241c57b3928337e8d8351878dc0cc0
Elvis <idsg@eecs.berkeley.edu>
2012-08-02 08:07:31 -0700

    Pylinted admin.py

--- bear_apps/store/admin.py
+++ bear_apps/store/admin.py
@@ -1,25 +1,28 @@
 from store.models import User_Apps, User, App, Group, Chartstring
 from django.contrib import admin
 
+
 class AppsInline(admin.TabularInline):
-	model = User_Apps
+    model = User_Apps
+
 
 class UsersInLine(admin.TabularInline):
-	model = User.groups.through
+    model = User.groups.through
+
 
 class ChartstringInline(admin.TabularInline):
-	model = Chartstring
+    model = Chartstring
+
 
 class UserAdmin(admin.ModelAdmin):
     fields = ['name', 'password', 'SID', 'user_type']
     inlines = [AppsInline, UsersInLine]
 
+
 class GroupAdmin(admin.ModelAdmin):
-	inlines = [UsersInLine]
+    inlines = [UsersInLine]
 
 admin.site.register(User, UserAdmin)
 admin.site.register(App)
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Chartstring)
-# admin.site.register(admin)
-# admin.site.register(User)
