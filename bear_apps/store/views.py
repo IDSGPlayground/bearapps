44b574dbacb2fa2318b3b151e11d30366ff9f97e
Ronald Lee <douglasc92@eecs.berkeley.edu>
2012-08-23 19:41:26 -0700

    updated stuff

--- bear_apps/store/views.py
+++ bear_apps/store/views.py
@@ -110,6 +110,12 @@ def register(request):
             try:
                 add_group = Group.objects.get(name=group)
             except ObjectDoesNotExist:
+                if user_type == "GENERAL" or user_type == "MANAGER":
+                    if group == '':
+                        con['blank_group'] = True
+                        new_user.delete()
+                        con.update(csrf(request))
+                        return render_to_response('register.html', con)
                 add_group = Group.objects.create(name=group)
             new_user.groups.add(add_group)
 
