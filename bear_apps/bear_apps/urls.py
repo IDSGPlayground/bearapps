79be71e2bbc39a205ed8a114a48317d80218e63d
Elvis <idsg@eecs.berkeley.edu>
2012-10-12 16:23:27 -0700

    added more features to the api

--- bear_apps/bear_apps/urls.py
+++ bear_apps/bear_apps/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from dajaxice.core import dajaxice_autodiscover
-from store.api import UserResource
+from store.api import UserResource, AppResource, ChartResource, GroupResource
 
 dajaxice_autodiscover()
 user_resource = UserResource()
+group_resource = GroupResource()
+chart_resource = ChartResource()
+app_resource = AppResource()
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -22,10 +25,12 @@ urlpatterns = patterns('',
       url(r'^backend/', include(admin.site.urls)),
       url(r'^admin/', 'store.views.admin', name='admin'),
       url(r'^register', 'store.views.register', name='register'),
-      url(r'^browse/', 'store.views.browse', name='browse' ),
+      url(r'^browse/', 'store.views.browse', name='browse'),
       url(r'^my-apps/', 'store.views.myapps', name='my-apps'),
       url(r'^manage/', 'store.views.manage', name='manage'),
       url(r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
-	  url(r'^api/', include(user_resource.urls)),
+      url(r'^api/', include(user_resource.urls)),
+      url(r'^api/', include(app_resource.urls)),
+      url(r'^api/', include(group_resource.urls)),
+      url(r'^api/', include(chart_resource.urls)),
 )
-
--- bear_apps/store/api.py
+++ bear_apps/store/api.py
@@ -1,10 +1,29 @@
-from tastypie.resource import Authorization
+# from tastypie.resources import Authorization
 from tastypie.resources import ModelResource
-from store.models import User
+from store.models import User, App, Chartstring, Group
 
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
-        resource_name = 'user'
-		authorization= Authorization()
+        resources_name = 'user'
+        # resource_name = 'api/user'
+        # authorization = Authorization()
+
+
+class AppResource(ModelResource):
+    class Meta:
+        queryset = App.objects.all()
+        resource_name = 'app'
+
+
+class ChartResource(ModelResource):
+    class Meta:
+        queryset = Chartstring.objects.all()
+        resource_name = 'chartstring'
+
+
+class GroupResource(ModelResource):
+    class Meta:
+        queryset = Group.objects.all()
+        resource_name = 'groups'
